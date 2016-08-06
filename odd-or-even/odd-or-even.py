f169dd023aaee2173ca24ecf7cc9b076aae5a822
vmnguyen <51302307@hcmut.edu.vn>
2016-08-06 22:42:17 +0700

    Finished extra problems

--- odd-or-even/odd-or-even.py
+++ odd-or-even/odd-or-even.py
@@ -2,9 +2,15 @@
 
 # Get a number from user,
 num = int(input("Give me a number: "))
-
+check = int(input("Give me a check number: "))
 # Check wherther input is odd or even
-if num % 2 == 0:
-	print ("The number you just entered is Odd")
-else: 
-	print ("The number you just entered is Even")
+if check % num == 0:
+	print ("The check number is devides evenly to num")
+else:
+	if num % 2 == 0:
+		if num % 4 == 0:
+			print ("The number you just entered is Mutilple of 4")
+		else:
+			print ("The number you just entered is Odd")
+	else: 
+		print ("The number you just entered is Even")
