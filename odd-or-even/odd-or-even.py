7df7302dae5780e276e4aa25568132e57d870ba5
vmnguyen <51302307@hcmut.edu.vn>
2016-08-06 22:44:56 +0700

    Change and add comments in odd-or-even

--- odd-or-even/odd-or-even.py
+++ odd-or-even/odd-or-even.py
@@ -3,11 +3,13 @@
 # Get a number from user,
 num = int(input("Give me a number: "))
 check = int(input("Give me a check number: "))
-# Check wherther input is odd or even
+# Check if check is devided evenly to num
 if check % num == 0:
 	print ("The check number is devides evenly to num")
 else:
+	# Check whether input is odd or even
 	if num % 2 == 0:
+		# Check if num is devided to 4
 		if num % 4 == 0:
 			print ("The number you just entered is Mutilple of 4")
 		else:
