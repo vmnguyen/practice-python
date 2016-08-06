625d452910f0ecf7c659b87187bc8378722f04eb
vmnguyen <51302307@hcmut.edu.vn>
2016-08-06 22:35:47 +0700

    Finished basic problem

---
+++ odd-or-even/odd-or-even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Get a number from user,
+num = int(input("Give me a number: "))
+
+# Check wherther input is odd or even
+if num % 2 == 0:
+	print ("The number you just entered is Odd")
+else: 
+	print ("The number you just entered is Even")
