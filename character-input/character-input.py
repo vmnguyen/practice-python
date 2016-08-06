c10d7199a18ff3b7d1a31b111c795363fa54c594
vmnguyen <51302307@hcmut.edu.vn>
2016-08-06 22:28:24 +0700

    Finished extras problem in character-input

--- character-input/character-input.py
+++ character-input/character-input.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-name = input("Give me your name:")
-age = int(input("Enter your age:"))
+name = input("Give me your name: ")
+age = int(input("Enter your age: "))
+repeat = int(input("How many time you want to copy messeges "))
 year = (2016-age) + 100
-print ("In the " + str(year) + ", you'll be 100 years old")
+for i in range(0,repeat):
+	print ("In the " + str(year) + ", you'll be 100 years old")
