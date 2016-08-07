1632dc33303e1cd258e83f7ecaeb20ef0413eaf8
vmnguyen <51302307@hcmut.edu.vn>
2016-08-07 23:01:49 +0700

    Finised Extra 1 problem

--- list-less-than-ten/listlessthanten.py
+++ list-less-than-ten/listlessthanten.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# This is the code for basic problem
+'''
 for i in a:
 	if i > 5:
 		print (i)
+'''
+
+# This is the code for problem Extra 1
+result = []
+for i in a:
+	if i > 5:
+		result.append(i)
+print result
