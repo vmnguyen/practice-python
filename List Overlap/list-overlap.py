82172803acf29a2e23a269e2c9a9ce025d72ea22
vmnguyen <51302307@hcmut.edu.vn>
2016-08-08 23:54:43 +0700

    Finished One line problem

--- List Overlap/list-overlap.py
+++ List Overlap/list-overlap.py
@@ -6,3 +6,9 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 for i in set(a):
 	if i in b:
 		print i,
+
+print 
+
+# Only one line problem
+result = [ x for x in set(a) if x in b ]
+print result
