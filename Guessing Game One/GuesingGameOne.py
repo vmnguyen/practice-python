25f81c80f503e58bcb2f3cd14a1343a154bb54f2
vmnguyen <51302307@hcmut.edu.vn>
2016-08-09 23:45:58 +0700

    Finished Guessing Game One problem

--- Guessing Game One/GuesingGameOne.py
+++ Guessing Game One/GuesingGameOne.py
@@ -3,14 +3,24 @@
 import random
 
 count = 0
-while True:
-	num = random.randInt(1,9)
-	guess = input("What number did you guess: ")
-	if guess = "exit":
-		break
-	guess = int(guess)
-	count = count + 1
-	if number == guess:
-		print "Not same"
+num = random.randint(1,9)
+flag = True
+while flag:
+	while True:
+		guess = input("What number did you guess: ")
+		if guess == "exit":
+			flag = False
+			break
+		guess = int(guess)
+		count = count + 1
+		if num == guess:
+			print ("Exactly\n")
+			print ("Type 'exit' to exit this game\n")
+			break
+		else:
+			if num < guess: 
+				print ("Too high")
+			if num > guess : 
+				print ("Too low")
 
-print ("You had taken " + str(count) + " times")
+print ("You had taken " + str(count) + " times")	
