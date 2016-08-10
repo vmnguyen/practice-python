2c85f953a1e3ae2f9200cdba7c9feab1f11531f5
vmnguyen <51302307@hcmut.edu.vn>
2016-08-10 23:20:09 +0700

    Finised Extra problem

--- List Remove Duplicates/ListRemoveDuplicates.py
+++ List Remove Duplicates/ListRemoveDuplicates.py
@@ -2,5 +2,15 @@
 #Author: bob
 def removeDuplicatesUsingSet(list):
 	return set(list)
+def removeDuplicatesUsingForLoop(list):
+	result = []
+	for i in list:
+		if i in result:
+			result
+		else:
+			result.append(i)
+	return result
+
 a = [1,1,5,6,2,4,5,2,8,5,10]
 print (removeDuplicatesUsingSet(a))
+print (removeDuplicatesUsingForLoop(a))
