3db048cb16f0e0119ff3e94fa230ad93984719d8
vmnguyen <51302307@hcmut.edu.vn>
2016-08-10 22:13:40 +0700

    Finished extra problem

--- List Overlap Comprehensions/ListOverlapComprehensions.py
+++ List Overlap Comprehensions/ListOverlapComprehensions.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python2
 #Author: bob
-
+import random
+'''
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+'''
+a = random.sample(range(1,100), random.randint(5,20))
+b = random.sample(range(1,100), random.randint(5,20))
 result = [i for i in set(a) if i in b]
 print result
