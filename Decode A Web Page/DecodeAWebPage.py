c389eab14d410f3b4822937f579a92c520bb0354
vmnguyen <51302307@hcmut.edu.vn>
2017-04-18 23:30:53 +0700

    Add code for Decode a webpage

--- Decode A Web Page/DecodeAWebPage.py
+++ Decode A Web Page/DecodeAWebPage.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 #Author: bob
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get('https://www.nytimes.com/')
+result = r.text
+soup = BeautifulSoup(result, 'html.parser')
+for i in soup.find_all(class_="story-heading"):
+	if i.a:
+		print (i.a.text.replace("\n", " ").strip())
+	else:
+		print (i.text.strip())
